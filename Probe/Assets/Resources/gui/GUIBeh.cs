6600eb64a544552fb3fe361fd9b304f49ae7789c
Tiempo8 <tiempo882@gmail.com>
2015-12-07 01:00:01 +0400

    chain

---
+++ Probe/Assets/Resources/gui/GUIBeh.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIBeh : MonoBehaviour {
+
+	GUISkin startSkin;
+	void Start () {
+		startSkin=(GUISkin) Resources.Load("gui/skins/button_start");
+	}
+	
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(20,20,100,30),"",startSkin.button))
+		{
+			//ACTION
+		}
+	}
+}
---
+++ Probe/Assets/Resources/scripts/BotBeh.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BotBeh : MonoBehaviour {
+
+	GameObject chainElement;
+	GameObject lastElement;
+	public GameObject groundElement;
+	void Start () {
+		int i;
+		lastElement=gameObject;
+		for(i=0;i<25;i++)
+		{
+			chainElement=(GameObject) Instantiate(Resources.Load("prefabs/chainElement"),transform.position+new Vector3(0,4.4f+0.25f*(i+1),0),Quaternion.identity);
+			lastElement.GetComponent<HingeJoint2D>().connectedBody=chainElement.GetComponent<Rigidbody2D>();
+			
+			lastElement=chainElement;
+		}
+		lastElement.GetComponent<HingeJoint2D>().connectedBody=groundElement.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
